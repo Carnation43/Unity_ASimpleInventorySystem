6c5faabcf4025dc9b04bd5b2aa98651fe9e73c87
Carnation43 <997551158@qq.com>
2025-09-27 12:07:14 +0800

    Added the Tooltip function. When an item is selected, different contents will be displayed according to the different types of the item.

---
+++ InventorySystemScripts/TooltipInstance.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TooltipInstance : MonoBehaviour
+{
+    public static TooltipInstance instance;
+
+    [Header("Components")]
+    [SerializeField] TMP_Text title;
+    [SerializeField] TMP_Text amount;
+    public Button equipButton;
+    public TMP_Text equipButtonText;
+
+    [Header("Components - stats")]
+    [SerializeField] GameObject attackGo;
+    [SerializeField] TMP_Text attackValue;
+    [SerializeField] TMP_Text attackCorrectionValue;        // Not set for the time being
+    [SerializeField] GameObject defenceGo;
+    [SerializeField] TMP_Text defenceValue;
+    [SerializeField] TMP_Text defenceCorrectionValue;       // Not set for the time being
+    [SerializeField] GameObject hpGo;
+    [SerializeField] TMP_Text hpValue;
+
+    [SerializeField] TMP_Text description;
+
+    CanvasGroup canvasGroup;
+
+    private RectTransform tooltipRectTransform;
+    public RectTransform _trackedRectTransform;
+
+    private Sequence currentSeq;
+
+    public bool IsHidden { get; private set; } = false;
+    public bool IsAnimating { get; private set; }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        canvasGroup = GetComponent<CanvasGroup>();
+        tooltipRectTransform = GetComponent<RectTransform>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    private void Update()
+    {
+     
+        if (_trackedRectTransform != null)
+        {
+            tooltipRectTransform.position = Vector3.Lerp(tooltipRectTransform.position, _trackedRectTransform.position, Time.deltaTime * 15);
+        }
+    }
+
+    public void Show(RectTransform _rectTransform)
+    {
+        if (_rectTransform == null) return;
+
+        _trackedRectTransform = _rectTransform;
+
+        if (IsHidden) return;
+
+        if (canvasGroup.alpha == 0)
+        {
+            tooltipRectTransform.position = _rectTransform.position;
+            canvasGroup.DOFade(1, 0.15f); 
+        }
+    }
+
+    public void Hide()
+    {
+        canvasGroup.DOFade(0, 0.15f);
+    }
+
+    public void ToggleTooltip()
+    {
+        if (_trackedRectTransform == null) return;
+        else
+            Debug.Log("Tracked!");
+
+        if (IsHidden)
+            ShowFull();
+        else
+            HideFull();
+    }
+
+    private void HideFull()
+    {
+        if (IsAnimating) return;
+        IsAnimating = true;
+        IsHidden = true;
+
+        currentSeq?.Kill();
+
+        currentSeq = DOTween.Sequence();
+        currentSeq.Append(canvasGroup.DOFade(0, 0.15f));
+        currentSeq.Join(tooltipRectTransform.DOScaleY(0, 0.5f).SetEase(Ease.InQuad));
+        currentSeq.OnComplete(() => { IsAnimating = false; });
+    }
+
+    private void ShowFull()
+    {
+        if (IsAnimating) return;
+        IsAnimating = true;
+        IsHidden = false;
+
+        currentSeq?.Kill();
+
+        tooltipRectTransform.localScale = new Vector3(1, 0, 1);
+        canvasGroup.alpha = 0;
+
+        tooltipRectTransform.position = _trackedRectTransform.position;
+
+        currentSeq = DOTween.Sequence();
+        currentSeq.Append(canvasGroup.DOFade(1, 0.15f));
+        currentSeq.Join(tooltipRectTransform.DOScaleY(1, 0.5f).SetEase(Ease.OutQuad));
+        currentSeq.OnComplete(() =>
+        {
+            IsAnimating = false;
+        });
+
+    }
+
+    public void setTooltip(InventorySlot slot)
+    {
+        title.text = slot.item.itemName;
+        amount.text = "Amount: " + slot.count.ToString();
+
+        // stats
+        attackGo.SetActive(slot.item.attack > 0);
+        attackValue.text = slot.item.attack.ToString();
+        defenceGo.SetActive(slot.item.defence > 0);
+        defenceValue.text = slot.item.defence.ToString();
+        hpGo.SetActive(slot.item.hp != 0);
+        if(slot.item.hp > 0)
+        {
+            hpValue.text = "+" + slot.item.hp.ToString();
+            hpValue.color = new Color(0, 0.6f, 0, 1);
+        }
+        else
+        {
+            hpValue.text = slot.item.hp.ToString();
+            hpValue.color = new Color(0.6f, 0, 0, 1);
+        }
+            
+        description.text = slot.item.GeneralDescription;
+
+        // Consumable = 1 | weapon = 2 | Equipment = 3 | Accessory = 4 | Material = 5 |
+
+        equipButton.gameObject.SetActive(true);
+        switch (slot.item.category)
+        {
+            case ItemCategory.Weapon:
+            case ItemCategory.Accessory:
+            case ItemCategory.Equipment: equipButtonText.text = "Equip (J)"; break;
+            case ItemCategory.Consumable: equipButtonText.text = "Consume (J)"; break;
+            case ItemCategory.Material: equipButtonText.text = "Craft (J)"; break;
+            default: equipButton.gameObject.SetActive(false); break;
+        }
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(tooltipRectTransform);
+    }
+
+}
